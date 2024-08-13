f4803a2abed89ba988ed8690874428483bcf8dfd
jvecodev <jvitor.oliveira1803@gmail.com>
2024-08-12 21:10:02 -0300

    Atualizando factory

--- exercícios.JS/POO/factoryFunction.js
+++ exercícios.JS/POO/factoryFunction.js
@@ -12,11 +12,13 @@ function Car(make, model, year) {
   const myCar = Car('Toyota', 'Corolla', 2020);
   console.log(myCar.getDetails()); // Saída: 2020 Toyota Corolla
 
-
+//bloco de cometarios para explicar o codigo acima
   /*
   Neste exemplo, a função Car atua como uma fábrica de objetos Car. Cada vez que você chama Car(), um novo objeto com as propriedades e métodos especificados é retornado.
+
   É um conceito que se adeque a POO, mas sem utilizar a sintaxe new e sem a necessidade de definir um construtor.
   */
+
 
 
   
